f91d901f98107a5995d2915801a64e2d538ad0ca
dlooz8 <mazurzoolda@gmail.com>
2024-09-26 20:48:43 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-3xl color-indigo-500">Gello</h1>
+            <Button>Click me</Button>
+        </div>
+    );
+}
